588712dd934c036545e03f6cfd28616aec8322e6
lijiamao <951572198@qq.com>
2023-08-07 10:29:58 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: 'https://ele-cat.gitee.io/vue3-wechat',
   server: {
     host: 'localhost',
     open: true,
