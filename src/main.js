2f59d76d6d68d413b8828f7463ffd8f8804abdd8
lijiamao <951572198@qq.com>
2023-08-09 13:56:38 +0800

    feat：修改打包配置

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from "./App.vue";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 import "./global.less";
-
+console.log(import.meta.env)
 const app = createApp(App);
 
 app.use(pinia);
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path"
-import viteCompression from 'vite-plugin-compression'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    viteCompression({
-      threshold: 1024000
-    })
-  ],
-  resolve: {
-    // ↓路径别名，主要是这部分
-    alias: {
-      "@": resolve(__dirname, "./src")
-    }
-  },
-  server: {
-    host: 'localhost',
-    open: true,
-    port: 9527,
-  },
-  // base: "https://ele-cat.gitee.io/vue3-wechat"
+export default defineConfig((mode) => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root)
+  console.log('mode: ', mode);
+  console.log('env1: ', env);
+  return {
+    plugins: [vue()],
+    resolve: {
+      // ↓路径别名，主要是这部分
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: "localhost",
+      open: env.VITE_OPEN,
+      port: env.VITE_PORT,
+    },
+    base: env.VITE_PUBLIC_PATH
+  }
 });
