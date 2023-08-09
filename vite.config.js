6c1418123f57fca47a781f4812cfc9a34fdcaabf
lijiamao <951572198@qq.com>
2023-08-09 11:38:58 +0800

    推送dist

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path"
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteCompression({
+      threshold: 1024000
+    })
+  ],
   resolve: {
     // ↓路径别名，主要是这部分
     alias: {
